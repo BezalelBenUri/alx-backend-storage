d3bb1cd393edeb72c0ad05250df17eea504891d1
PhenomenalAI <diamondinyang@gmail.com>
2024-01-24 10:47:30 -0800

    5. Implementing an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,41 +1,36 @@
 #!/usr/bin/env python3
 """
-The core of the function is very simple. It uses the requests module
-to obtain the HTML content of a particular URL and returns it
-web cache and tracker
+The core of the function is very simple. It uses the requests
+module to obtain the HTML content of a particular URL and returns it
+Redis Module
 """
-import requests
-import redis
+
 from functools import wraps
+import redis
+import requests
+from typing import Callable
 
-store = redis.Redis()
+redis_ = redis.Redis()
 
 
-def count_url_access(method):
-    """ Decorator counting how many times
-    a URL is accessed """
+def count_requests(method: Callable) -> Callable:
+    """ Decortator for counting """
     @wraps(method)
-    def wrapper(url):
-        """
-        decorator's wrapper
-        """
-        cached_key = "cached:" + url
-        cached_data = store.get(cached_key)
-        if cached_data:
-            return cached_data.decode("utf-8")
-
-        count_key = "count:" + url
+    def wrapper(url):  # sourcery skip: use-named-expression
+        """ Wrapper for decorator """
+        redis_.incr(f"count:{url}")
+        cached_html = redis_.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
         html = method(url)
-
-        store.incr(count_key)
-        store.set(cached_key, html)
-        store.expire(cached_key, 10)
+        redis_.setex(f"cached:{url}", 10, html)
         return html
+
     return wrapper
 
 
-@count_url_access
+@count_requests
 def get_page(url: str) -> str:
-    """ Returns HTML content of a url """
-    res = requests.get(url)
-    return res.text
+    """ Obtain the HTML content of a  URL """
+    req = requests.get(url)
+    return req.text
