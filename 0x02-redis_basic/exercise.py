94eebb0c708618190caf64eda7550110a9a4153e
PhenomenalAI <diamondinyang@gmail.com>
2024-01-24 10:30:34 -0800

    corrections

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,102 +1,104 @@
 #!/usr/bin/env python3
 """
 Cache module using Redis.
+ Writing strings to Redis
+Reading from Redis and recovering original type
+Incrementing values, storing lists, Retrieving list
 
-Author: Your Name
+Author: PhenomenalAI
 """
-
-import uuid
-from typing import Union
 import redis
+import uuid
+from typing import Union, Callable, Optional
 from functools import wraps
 
 
 def count_calls(method: Callable) -> Callable:
     """
-    Decorator to count how many times a method of the Cache class is called.
-
-    Args:
-        method (Callable): The method to be decorated.
-
-    Returns:
-        Callable: The decorated method.
-    """
-    @wraps(method)
-    def wrapper(self, *args, **kwargs):
-        # Generate the key using the qualified name of the method
-        key = f"{method.__qualname__}_calls"
-
-        # Increment the count for the key in Redis
-        self._redis.incr(key)
-
-        # Call the original method and return its result
-        return method(self, *args, **kwargs)
-
-    return wrapper
-
-class Cache:
-    """
-    Cache class using Redis.
-
-    This class provides a simple interface for storing data in Redis with
-    randomly generated keys.
-
+    Prototype: def count_calls(method: Caallable) -> Callable:
+    Returns a Callable
     Author: PhenomenalAI
     """
-
-    def __init__(self) -> None:
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
         """
-        Initialize the Cache instance.
-
-        It creates a Redis client and flushes the database.
-
-        Returns:
-            None
+        Prototype: def wrapper(self, *args, **kwds):
+        Returns wrapper
         """
-        # Create a Redis client
+        key_m = method.__qualname__
+        self._redis.incr(key_m)
+        return method(self, *args, **kwds)
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """"
+    Prototype: def call_history(method: Callable) -> Callable:
+    Returns a Callable
+    Author: PhenomenalAI
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
+        """
+        Prototype: def wrapper(self, *args, **kwds):
+        Returns wrapper
+        """
+        key_m = method.__qualname__
+        inp_m = key_m + ':inputs'
+        outp_m = key_m + ":outputs"
+        data = str(args)
+        self._redis.rpush(inp_m, data)
+        fin = method(self, *args, **kwds)
+        self._redis.rpush(outp_m, str(fin))
+        return fin
+    return wrapper
+
+
+def replay(func: Callable):
+    """
+    Prototype: def replay(func: Callable):
+    Displays history of calls of a particular function
+    Author: PhenomenalAI
+    """
+    r = redis.Redis()
+    key_m = func.__qualname__
+    inp_m = r.lrange("{}:inputs".format(key_m), 0, -1)
+    outp_m = r.lrange("{}:outputs".format(key_m), 0, -1)
+    calls_number = len(inp_m)
+    times_str = 'times'
+    if calls_number == 1:
+        times_str = 'time'
+    fin = '{} was called {} {}:'.format(key_m, calls_number, times_str)
+    print(fin)
+    for k, v in zip(inp_m, outp_m):
+        fin = '{}(*{}) -> {}'.format(
+            key_m, k.decode('utf-8'), v.decode('utf-8'))
+        print(fin)
+
+
+class Cache():
+    """
+    Store instance of Redis client as private variable _redis
+    Flush the instance using flushdb
+    Author: PhenomenalAI
+    """
+    def __init__(self):
+        """
+        Prototype: def __init__(self):
+        Store instance of Redis client as private variable _redis
+        """
         self._redis = redis.Redis()
-
-        # Flush the database
         self._redis.flushdb()
 
+    @call_history
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
-        Store data in Redis using a randomly generated key.
-
-        Args:
-            data (Union[str, bytes, int, float]): The data to be stored.
-
-        Returns:
-            str: The randomly generated key used for storing the data.
+        Store history of inputs and outputs for a particular function
         """
-        # Generate a random key using uuid
-        key = str(uuid.uuid4())
-
-        # Store the data in Redis using the generated key
-        self._redis.set(key, data)
-
-        # Return the generated key
-        return key
-
-
-    def store(self, data: Union[str, bytes, int, float]) -> str:
-        """
-        Store data in Redis using a randomly generated key.
-
-        Args:
-            data (Union[str, bytes, int, float]): The data to be stored.
-
-        Returns:
-            str: The randomly generated key used for storing the data.
-        """
-        # Generate a random key using uuid
-        key = str(uuid.uuid4())
-
-        # Store the data in Redis using the generated key
-        self._redis.set(key, data)
-
-        # Return the generated key
-        return key
+        gen = str(uuid.uuid4())
+        self._redis.set(gen, data)
+        return gen
 
     def get(self, key: str,
             fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
